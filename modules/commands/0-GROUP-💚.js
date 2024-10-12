ed2def224f4a52a5d94e4a840bb8aa193be43af0
ANSARI HU PAGAL 😁 <sonukhumar835@gmail.com>
2024-10-12 17:24:38 +0530

    Update 0-GROUP-💚.js

--- modules/commands/0-GROUP-💚.js
+++ modules/commands/0-GROUP-💚.js
@@ -1,32 +1,30 @@
-
-
 const fs = require("fs");
 module.exports.config = {
-	name: "group",
-    version: "1.0.1",
-	hasPermssion: 0,
-	credits: "faiz", 
-	description: "no prefix",
-	commandCategory: "No command marks needed",
-	usages: "...",
-    cooldowns: 1, 
+  name: "group",
+    version: "2.1.1",
+  hasPermssion: 0,
+  credits: "FAIZ ANSARI", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	let react = event.body.toLowerCase();
-	if(react.includes("group") ||
-     react.includes("Group") || 
-react.includes("GROUP")) {
-		var msg = {
-				body: "●▬▬๑💖𝙁𝘼𝙄𝙕 𝘼𝙉𝙎𝘼𝙍𝙄💖 ✶𝐌𝐘 𝐁𝐎𝐒𝐒✶๑▬▬😍😍😍 .... 😊😊😊💖𝗝𝗢𝗜𝗡🗡️𝗠𝗘𝗥𝗘 𝗚𝗥𝗢𝗨𝗣 𝗠𝗘 𝗔𝗔𝗢 𝗘𝗡𝗝𝗢𝗬 𝗞𝗔𝗥𝗢💖 ➻👉🏻♦️𝙂𝙍𝙊𝙐𝙋👉𝙇𝙄𝙉𝙆  https://m.me/j/AbYZg4030gC9XmjT/ ✨",
-				attachment: fs.createReadStream(__dirname + `/FAIZ/group.jpeg`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("💚", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
+  var name = await Users.getNameUser(event.senderID);
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("group") ||
+     react.includes("GROUP") || react.includes("Group") || react.includes("Grop") ||
+react.includes("Grop") ||
+react.includes("GROPP") ||     
+react.includes("Gropp")) {
+    var msg = {
+        body: `${name} 🦢🍒❤️▬▬๑💖𝙁𝘼𝙄𝙕 𝘼𝙉𝙎𝘼𝙍𝙄💖 ✶𝐌𝐘 𝐁𝐎𝐒𝐒✶๑▬▬😍😍😍 .... 😊😊😊💖𝗝𝗢𝗜𝗡🗡️𝗠𝗘𝗥𝗘 𝗚𝗥𝗢𝗨𝗣 𝗠𝗘 𝗔𝗔𝗢 𝗘𝗡𝗝𝗢𝗬 𝗞𝗔𝗥𝗢💖 ➻👉🏻♦️𝙂𝙍𝙊𝙐𝙋👉𝙇𝙄𝙉𝙆  https://m.me/j/AbYZg4030gC9XmjT/ ✨✿`,attachment: fs.createReadStream(__dirname + `/noprefix/FAIZ/group.jpeg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("💚", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
 
-  }
-
+  }
